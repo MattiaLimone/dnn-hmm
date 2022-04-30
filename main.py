ac8de3597e35b45fd254ccb61ad4ae5ecc8d7a0c
Mattia Limone <m.limone7@studenti.unisa.it>
2022-04-30 16:35:41 +0200

    LPC. MFCC. PCA over LFC, PCA over MFCC

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 import os
 import numpy as np
 import librosa
+import librosa.display
 from pydub import AudioSegment, silence
 import soundfile as sf
+from sklearn.decomposition import PCA
 
+# Function to detect and remove the silence intervals where silence
+# last 500ms and decibel range reduction is higher than 16dB
 def remove_silence(path, export_path = 'export/'):
     # Check if export path exist
     if not os.path.exists(export_path):
@@ -29,3 +33,82 @@ def remove_silence(path, export_path = 'export/'):
     # Save wav file
     sf.write(export_path + filename, data, samplerate)
 
+    return data, samplerate
+# Function to extract Mel Frequency Cepstral Coefficient and first order and second order mfcc
+def extract_mfcc(signal, sr, order = 0):
+    mfccs = librosa.feature.mfcc(y=signal, n_mfcc=13, sr=sr)
+    delta2_mfccs = librosa.feature.delta(mfccs, order=2)
+    delta_mfccs = librosa.feature.delta(mfccs)
+    mfccs_features = np.concatenate((mfccs, delta_mfccs, delta2_mfccs))
+    if(order == 0):
+        return mfccs
+    if(order == 1):
+        return mfccs_features
+    if (order == 2):
+        return mfccs_features
+    '''
+    #Plot Mfccs
+    plt.figure(figsize=(25, 10))
+    librosa.display.specshow(mfccs,
+                             x_axis="time",
+                             sr=sr)
+    plt.colorbar(format="%+2.f")
+    plt.show()
+    # Plot first derivative Mfccs
+    plt.figure(figsize=(25, 10))
+    librosa.display.specshow(delta2_mfccs,
+                             x_axis="time",
+                             sr=sr)
+    plt.colorbar(format="%+2.f")
+    plt.show()
+    # Plot second derivative Mfccs
+    plt.figure(figsize=(25, 10))
+    librosa.display.specshow(delta_mfccs,
+                             x_axis="time",
+                             sr=sr)
+    plt.colorbar(format="%+2.f")
+    plt.show()
+    '''
+# Function to perform PCA over MFCC
+def extract_mfcc_pca_features(mfcc_features):
+    flattend_mfcc = np.array(mfcc_features)
+    flattend_mfcc = flattend_mfcc.transpose()
+    # initialize the pca
+    pca = PCA(n_components=1)
+    # fit the features in the model
+    pca.fit(flattend_mfcc)
+    # apply PCA and keep just one column, which means one feature vector with 13 features
+    sample = pca.transform(flattend_mfcc)
+    # reshape feature vector
+    sample = sample.transpose()
+    # transform it to a list in order to satisfy the format for writing the feature vector in the file
+    pca_features = sample.tolist()
+    # and keep just the first list, because it returns you a list of lists with only one list
+    pca_features = pca_features[0]
+    return pca_features
+# Function to extract Linear Predictor Coefficient of n order, default = 2
+def extract_lpc(signal, order = 2):
+    lpc_features = librosa.lpc(signal, order=order)
+    return lpc_features
+# Function to perform PCA over LPC
+def extract_lpc_pca_features(lpc_features):
+    flattend_lpc = np.array(lpc_features)
+    flattend_lpc = flattend_lpc.transpose()
+    # initialize the pca
+    pca = PCA(n_components=1)
+    # fit the features in the model
+    pca.fit(flattend_lpc)
+    # apply PCA and keep just one column, which means one feature vector with 13 features
+    sample = pca.transform(flattend_lpc)
+    # reshape feature vector
+    sample = sample.transpose()
+    # transform it to a list in order to satisfy the format for writing the feature vector in the file
+    pca_features = sample.tolist()
+    # and keep just the first list, because it returns you a list of lists with only one list
+    pca_features = pca_features[0]
+    return pca_features
+
+data, sr = remove_silence('data/lisa/data/timit/raw/TIMIT/TEST/DR1/FAKS0/SA1.WAV')
+mfcc_feature = extract_mfcc(data, sr)
+lpc_feature = extract_lpc(data, 5)
+pca_features = extract_mfcc_pca_features(mfcc_feature)
