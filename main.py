9dfba0718e8a85f6388534d6a635ba5d3a6693de
carmineiannotti <carmineiannotti@gmail.com>
2022-05-04 11:27:42 +0200

    LPC and LPCC feature extraction in main.py

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
-import os
-from glob import glob
+from spafe.utils import vis
+
 import preprocessing.utils as utl
-import pandas as pd
-from tqdm.auto import tqdm
-from typing import final
+from spafe.features.lpc import lpc, lpcc
 
-MEL_COEF: final = 13
+# init input vars
+num_ceps = 13
+lifter = 0
+normalize = True
 
-results = [y for x in os.walk("data/lisa/data/timit/raw/TIMIT/TRAIN") for y in glob(os.path.join(x[0], '*.WAV'))]
+# read wav
+sig, fs = utl.remove_silence('data/lisa/data/timit/raw/TIMIT/TEST/DR1/FAKS0/SA1.WAV')
+#fs, sig = scipy.io.wavfile.read("data/lisa/data/timit/raw/TIMIT/TEST/DR1/FAKS0/SA1.WAV")
 
-df_mfcc = pd.DataFrame(columns=[i for i in range(0, MEL_COEF*3*89)])
+# compute lpcs
+lpcs = lpc(sig=sig, fs=fs, num_ceps=num_ceps)
+# visualize features
+vis.visualize_features(lpcs, 'LPC Index', 'Frame Index')
 
-for path in tqdm(results):
-    data, sr = utl.remove_silence(path=path, export_path="data/cleaned/train/")
-    mfcc = utl.extract_mfcc(signal=data, sr=sr, n_mfcc=MEL_COEF)
-    mfcc_flatten = mfcc.flatten(order='F')
-    print(mfcc)
-    print(type(mfcc))
-    print(mfcc.shape)
-    df_mfcc.loc[-1] = mfcc_flatten
+
+# visualize spectogram
+vis.spectogram(sig, fs)
+# compute lpccs
+lpccs = lpcc(sig=sig, fs=fs, num_ceps=num_ceps, lifter=lifter, normalize=normalize)
+# visualize features
+vis.visualize_features(lpccs, 'LPCC Index', 'Frame Index')
+
+print('LPC')
+print(lpcs)
+print('\n')
+print('LPCC')
+print(lpccs)
+print(len(lpccs[0]))
+
 
 
