93df407ec63c49589f9313ca98ce101209620d18
Mattia Limone <m.limone7@studenti.unisa.it>
2022-05-26 13:10:58 +0200

    Added comment, return type, and raise error function.

--- preprocessing/features/lpcc.py
+++ preprocessing/features/lpcc.py
@@ -10,9 +10,9 @@ def extract_lpcc(signal: np.ndarray, sr: int, n_lpcc: int = LPCC_NUM_DEFAULT) ->
     """
         Extracts lpccs for each frame of the given audio.
 
-        :param signal: 1 x N audio signal to extract lpccs from.
-        :param sr: sample rate of audio file to construct audio frames.
-        :param n_lpcc: number of lpccs to compute (default LPCC_NUM_DEFAULT)
+        :param signal: A Numpy Array. 1 x N audio signal to extract lpccs from.
+        :param sr: An integer. The sample rate of audio file to construct audio frames.
+        :param n_lpcc: An integer. The number of lpccs to compute (default LPCC_NUM_DEFAULT)
         :return: a N x n_lpcc matrix containing lpccs for each of the given audio frames (created by sampling
                  with sampling rate sr), where N = number of constructed frames.
         """
@@ -20,6 +20,8 @@ def extract_lpcc(signal: np.ndarray, sr: int, n_lpcc: int = LPCC_NUM_DEFAULT) ->
         raise ValueError("Sampling rate must be positive.")
     if signal.ndim != 1:
         raise ValueError("Signal must be a 1 x N mono-dimensional array.")
+    if n_lpcc <= 0:
+        raise ValueError("Number of LPCCs must be strictly positive")
 
     lpccs = lpcc(sig=signal, fs=sr, num_ceps=n_lpcc, lifter=_LIFTER, normalize=_NORMALIZE)
     return np.array(lpccs)
--- preprocessing/features/mfcc.py
+++ preprocessing/features/mfcc.py
@@ -8,17 +8,25 @@ DERIVATIVE_ORDER_DEFAULT: final = 2
 
 
 # Function to extract Mel Frequency Cepstral Coefficient and first order and second order mfcc
-def extract_mfcc(signal: np.ndarray, sr: int, n_mfcc=MFCC_NUM_DEFAULT, order=DERIVATIVE_ORDER_DEFAULT):
+def extract_mfcc(signal: np.ndarray, sr: int, n_mfcc=MFCC_NUM_DEFAULT, order=DERIVATIVE_ORDER_DEFAULT) -> np.ndarray:
     """
     Extracts mfccs for each frame of the given audio.
 
-    :param signal: 1 x N audio signal to extract mfccs from.
-    :param sr: sample rate of audio file to construct audio frames.
-    :param n_mfcc: number of mfccs to compute (default MFCC_NUM_DEFAULT)
-    :param order: derivative order to compute for each extracted mfcc.
+    :param signal: A Numpy Array. 1 x N audio signal to extract mfccs from.
+    :param sr: An integer. The sample rate of audio file to construct audio frames.
+    :param n_mfcc: An integer. The number of mfccs to compute (default MFCC_NUM_DEFAULT)
+    :param order: An integer. The derivative order to compute for each extracted mfcc.
     :return: a N x (n_mfcc*order) matrix containing mfccs for each of the given audio frames (created by sampling with
              sampling rate sr), where N = number of constructed frames.
     """
+    if sr < 0:
+        raise ValueError("Sampling rate must be positive.")
+    if signal.ndim != 1:
+        raise ValueError("Signal must be a 1 x N mono-dimensional array.")
+    if n_mfcc <= 0:
+        raise ValueError("Number of MFCCs must be strictly positive")
+    if order < 0:
+        raise ValueError("The derivative order must be positive")
 
     mfccs = librosa.feature.mfcc(y=signal, n_mfcc=n_mfcc, sr=sr)
     mfccs_and_deltas = np.copy(mfccs)
