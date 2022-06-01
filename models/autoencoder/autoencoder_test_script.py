af8b0659de700dc88412d5717d0fb1b883f17517
Mattia Limone <m.limone7@studenti.unisa.it>
2022-06-01 21:34:04 +0200

    fix to autoencoder

--- models/autoencoder/autoencoder_test_script.py
+++ models/autoencoder/autoencoder_test_script.py
@@ -1,3 +1,4 @@
 from recurrent_autoencoder import RecurrentAutoEncoder
 
-model = RecurrentAutoEncoder(n_encoding_layers=3, input_neurons=64, timesteps=150, n_features=39, unit_type='GRU')
+model = RecurrentAutoEncoder(n_encoding_layers=3, sequential_bottleneck=False, input_neurons=256, timesteps=150, n_features=39, unit_type='LSTM')
+model.summary()
