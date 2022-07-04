3e5056fa647a4161e50b0012acee61c20c031a40
carmine98 <carmineiannotti@gmail.com>
2022-07-04 16:52:50 +0200

    Allign lpcc with main

--- preprocessing/features/lpcc.py
+++ preprocessing/features/lpcc.py
@@ -1,18 +1,16 @@
 from typing import final
 import numpy as np
 import torch
-from lpctorch import LPCCoefficients
-
+import lpctorch
 
 LPCC_NUM_DEFAULT: final = 13
 _FRAME_DURATION: final = .016
-_FRAME_OVERLAP: final = .5
+_FRAME_OVERLAP: final = 0.5
 
 
 def extract_lpccs(signal: np.ndarray, sr: int, n_lpcc: int = LPCC_NUM_DEFAULT) -> np.ndarray:
     """
         Extracts LPCCs for each frame of the given audio.
-
         :param signal: A Numpy Array. 1 x N audio signal to extract LPCCs from.
         :param sr: An integer. The sample rate of audio file to construct audio frames.
         :param n_lpcc: An integer. The number of LPCCs to compute (default LPCC_NUM_DEFAULT)
@@ -26,21 +24,20 @@ def extract_lpccs(signal: np.ndarray, sr: int, n_lpcc: int = LPCC_NUM_DEFAULT) -
     if n_lpcc <= 0:
         raise ValueError("Number of LPCCs must be strictly positive")
 
-
+    # Convert input signal from numpy array to PyTorch tensor
     signal = torch.from_numpy(signal.reshape(-1, 1).transpose())
 
     # lpccs = lpcc(sig=signal, fs=sr, num_ceps=n_lpcc, win_type="hamming", lifter=_LIFTER, normalize=_NORMALIZE)
 
-    lpc_prep = LPCCoefficients(
+    # Extract LPCCs using LPCTorch library
+    lpc_prep = lpctorch.LPCCoefficients(
         sr,
         _FRAME_DURATION,
         _FRAME_OVERLAP,
         order=(n_lpcc - 1)
     )
-
     alphas = lpc_prep(signal)
     alphas = alphas.cpu().detach().numpy()
-
     lpccs = alphas[0]
 
     return lpccs
