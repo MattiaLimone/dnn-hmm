ad51537dd12839b3b1132208809d3722badd0c92
Attornado <73835871+Attornado@users.noreply.github.com>
2022-07-07 09:23:10 +0200

    Added train script for LSTM model and updated recconvsnet_train_script.py.
    Signed-off-by: Attornado <73835871+Attornado@users.noreply.github.com>

--- training/lstm_predictor_train_script.py
+++ training/lstm_predictor_train_script.py
@@ -1,20 +1,18 @@
 from typing import final
 import tensorflow as tf
 from tensorflow.python.keras.callbacks import EarlyStopping
-from tensorflow.python.keras.optimizers import Adadelta
+from tensorflow.python.keras.optimizers import Adam, Adadelta
 import keras.regularizers as regularizers
 from matplotlib import pyplot
-from models.recconvsnet.recconvsnet import RecConv1DSiameseNet
+from keras.layers import Dense, Flatten, MaxPooling2D, Conv2D, BatchNormalization, Input, Dropout, \
+    TimeDistributed
 from models.autoencoder.autoencoder import ENCODER_MODEL_NAME
 from training.training_utils import TRAIN_SET_PATH_MFCCS, TEST_SET_PATH_MFCCS, TRAIN_SET_PATH_MEL_SPEC, \
     TEST_SET_PATH_MEL_SPEC, load_dataset, get_label_number, one_hot_labels_to_integer_labels
 
 
-_EPOCHS_LOAD_CONV: final = 750
 _EPOCHS_LOAD_REC: final = 1000
-_VERSION_LOAD_CONV: final = 1.1
 _VERSION_LOAD_REC: final = 1.0
-_CONV_AUTOENC_PATH: final = f"fitted_autoencoder/cnn/autoencoder_cnn_{_EPOCHS_LOAD_CONV}_epochs_v{_VERSION_LOAD_CONV}"
 _REC_AUTOENC_PATH: final = f"fitted_autoencoder/lstm/autoencoder_lstm_{_EPOCHS_LOAD_REC}_epochs_v{_VERSION_LOAD_REC}"
 
 
@@ -22,21 +20,16 @@ def main():
     # Load dataset and labels
     train_mfccs, train_mfccs_labels = load_dataset(TRAIN_SET_PATH_MFCCS)
     test_mfccs, test_mfccs_labels = load_dataset(TEST_SET_PATH_MFCCS)
-    train_mel_spec, train_mel_spec_labels = load_dataset(TRAIN_SET_PATH_MEL_SPEC)
-    test_mel_spec, test_mel_spec_labels = load_dataset(TEST_SET_PATH_MEL_SPEC)
     total_state_number = get_label_number(train_mfccs_labels)
 
-    # Load saved models
-    conv_autoencoder = tf.keras.models.load_model(_CONV_AUTOENC_PATH)
+    # Load saved model
     rec_autoencoder = tf.keras.models.load_model(_REC_AUTOENC_PATH)
 
     # Get input shapes
-    input_shape_conv_branch = (None,) + train_mel_spec.shape[1:]
     input_shape_rec_branch = (None,) + train_mfccs.shape[1:]
 
-    # Get recurrent and convolutional encoder layers
-    conv_branch = conv_autoencoder.get_layer(ENCODER_MODEL_NAME).layers
-    rec_branch = rec_autoencoder.get_layer(ENCODER_MODEL_NAME).layers
+    # Get recurrent encoder layers
+    rec_branch = rec_autoencoder.get_layer(ENCODER_MODEL_NAME)
 
     # Set model parameters
     tail_dense_units = 512
@@ -50,7 +43,7 @@ def main():
         name='sparse_categorical_crossentropy'
     )
     optimizer = Adadelta(
-        learning_rate=0.9,
+        learning_rate=1,
         rho=0.95,
         epsilon=1e-7,
         name='adadelta_optimizer'
@@ -63,37 +56,26 @@ def main():
     )]
     version = 0.3  # For easy saving of multiple model versions
 
-    # Instantiate the model and  ahcompile it
-    # era regularizers.L1L2(l1=1e-04, l2=1e-03)
-    # era regularizers.L1(1e-04)
-    model = RecConv1DSiameseNet(
-        rec_branch_layers=rec_branch,
-        conv_branch_layers=conv_branch,
-        input_shape_rec_branch=input_shape_rec_branch,
-        input_shape_conv_branch=input_shape_conv_branch,
-        tail_dense_units=tail_dense_units,
-        output_dim=total_state_number,
-        tail_dense_activation='relu',
-        add_repeat_vector_conv_branch=True,
-        kernel_regularizer_softmax=regularizers.L1(l1=0.001),
-        dropout_dense=0.6
-    )
+    # Instantiate the model and compile it
+    model = rec_branch
+    model.add(Dense(total_state_number, activation='softmax'))
+    model.build(input_shape_rec_branch)
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
     model.summary(expand_nested=True)
 
     # Convert one-hot encoded labels to integer labels
-    labels_train = one_hot_labels_to_integer_labels(train_mel_spec_labels)
-    labels_test = one_hot_labels_to_integer_labels(test_mel_spec_labels)
+    labels_train = one_hot_labels_to_integer_labels(train_mfccs_labels)
+    labels_test = one_hot_labels_to_integer_labels(test_mfccs_labels)
 
     # Train the model
     history = model.fit(
-        x=[train_mfccs, train_mel_spec],
+        x=train_mfccs,
         y=labels_train,
         epochs=epochs,
         batch_size=batch_size,
         shuffle=True,
         callbacks=callbacks,
-        validation_data=([test_mfccs, test_mel_spec], labels_test)
+        validation_data=(test_mfccs, labels_test)
     )
 
     # Plot results loss
@@ -107,9 +89,8 @@ def main():
     pyplot.plot(history.history['val_sparse_categorical_accuracy'], label='test parse categorical accuracy')
     pyplot.legend()
     pyplot.show()
-
     # Save the model to file
-    model.save(f'fitted_recconvsnet/recconvsnet_{epochs}_epochs_v{version}')
+    model.save(f'fitted_recconvsnet/lstm_predictor_{epochs}_epochs_v{version}')
 
 
 if __name__ == "__main__":
