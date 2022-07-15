88c194716b11b2ed4a1255b694c2cd8651200fcc
Luca <strluca94@gmail.com>
2022-07-15 22:44:37 +0200

    New dnnhmm_test_script.py

--- models/dnnhmm/dnnhmm_test_script.py
+++ models/dnnhmm/dnnhmm_test_script.py
@@ -9,27 +9,22 @@ from preprocessing.constants import UNSPLITTED_SET_PATH_MFCCS, TRAIN_SET_PATH_MF
 from training.training_utils import sparse_top_k_categorical_speaker_accuracy_mfccs, \
     speaker_n_states_in_top_k_accuracy_mfccs, sparse_categorical_speaker_accuracy_mfccs
 
-
 _EPOCHS_LOAD_RECCONV: final = 1
 _VERSION_LOAD_RECCONV: final = 0.4
 _RECCONV_NET_PATH: final = f"fitted_mlp_predictor/mlp_predictor_{_EPOCHS_LOAD_RECCONV}_epochs_v{_VERSION_LOAD_RECCONV}"
 
-
 def _compute_state_frequencies(labels: np.ndarray, audios_per_speaker: int = AUDIO_PER_SPEAKER) \
         -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-
     states, state_frequencies = np.unique(labels, return_counts=True)
     state_relative_frequencies = state_frequencies / (labels.shape[1]*audios_per_speaker)
     return states, state_frequencies, state_relative_frequencies
-
 
 def main():
     # Load dataset
     train_mfccs, train_mfccs_labels = load_dataset(TRAIN_SET_PATH_MFCCS)
     test_mfccs, test_mfccs_labels = load_dataset(TEST_SET_PATH_MFCCS)
-
     mfccs, mfccs_labels = load_dataset(UNSPLITTED_SET_PATH_MFCCS)
-    
+
     mfccs_labels = one_hot_labels_to_integer_labels(mfccs_labels)
 
     speaker_indexes = generate_or_load_speaker_ordered_dict()
@@ -42,7 +37,8 @@ def main():
         "sparse_top_k_categorical_speaker_accuracy_mfccs":sparse_top_k_categorical_speaker_accuracy_mfccs,
         "speaker_n_states_in_top_k_accuracy_mfccs":speaker_n_states_in_top_k_accuracy_mfccs,
         "sparse_categorical_speaker_accuracy_mfccs":sparse_categorical_speaker_accuracy_mfccs})
-
+    # Index for audio range 0:10 first speaker etc...
+    audio_range = 0
     for speaker in speaker_indexes:
         # Calculate state range
         start_range = speaker_indexes[speaker]*N_STATES_MFCCS
@@ -58,8 +54,7 @@ def main():
             emission_model=model,
             state_frequencies=speaker_state_frequencies
         )
-        # Index for audio range 0:10 first speaker etc...
-        audio_range = 0
+
         #iterate over the audio tensors
         for index in range(audio_range, audio_range+10):
             # qui c'è un problema con le dimensioni
@@ -73,6 +68,8 @@ def main():
             print(most_likely_path_prob)
             print("Label calculated")
             print(state_frequencies[index])
+
+        audio_range += 10
 
 if __name__ == "__main__":
     main()
